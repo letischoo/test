c1d7ff689cee796482edeef553b62fd3225e40ba
Dominik Forys <letischoo@gmail.com>
2014-10-23 17:59:19 +0200

    basic firewall

--- web/index.php
+++ web/index.php
@@ -3,8 +3,27 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+	'game' => array(
+		'pattern' => '^/game',
+		'http' => true,
+		'users' => array(
+			'admin' => array('ROLE_ADMIN','5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+			),
+		),
+	)
+));
+
+
+
 $app->get('/hello/{name}', function($name) use($app) {
 	return 'Hello '.$app->escape($name);
 });
+
+$app->get('/game', function() use($app) {
+	return 'thisisgame';
+});
+
 
 $app->run();
