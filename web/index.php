2d6e83dd433613e76878f9f0ca8fbe455d1860db
Dominik Forys <letischoo@gmail.com>
2014-10-23 18:17:59 +0200

    improved firewall

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     'security.firewalls' => array(
 	'game' => array(
 		'pattern' => '^/game',
-		'http' => true,
+		'form' => array('login_path' => '/login', 'check_path' => '/game/login_check'),
 		'users' => array(
 			'admin' => array('ROLE_ADMIN','5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
 			),
@@ -17,12 +17,17 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
 
 
 
+
 $app->get('/hello/{name}', function($name) use($app) {
 	return 'Hello '.$app->escape($name);
 });
 
 $app->get('/game', function() use($app) {
 	return 'thisisgame';
+});
+
+$app->get('/login', function() use($app) {
+	return 'login form';
 });
 
 
