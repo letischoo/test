527b25e6dbe129e1d434d5466d4aac982f47a3f4
Dominik Forys <letischoo@gmail.com>
2014-10-30 18:17:02 +0100

    robienie logowania i wylogowywania + twig

--- web/index.php
+++ web/index.php
@@ -1,34 +1,53 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     'security.firewalls' => array(
-	'game' => array(
-		'pattern' => '^/game',
-		'form' => array('login_path' => '/login', 'check_path' => '/game/login_check'),
-		'users' => array(
-			'admin' => array('ROLE_ADMIN','5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+		'game' => array(
+			'pattern' => '^/game',
+			'form' => array('login_path' => '/login', 'check_path' => '/game/login_check'),
+			'logout' => array('logout_path' => '/game/logout'),
+			'users' => array(
+				'admin' => array('ROLE_ADMIN','5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+				),
 			),
-		),
+		)
 	)
+);
+
+
+
+$app['debug'] = true;
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
 ));
 
 
-
-
-$app->get('/hello/{name}', function($name) use($app) {
-	return 'Hello '.$app->escape($name);
-});
-
 $app->get('/game', function() use($app) {
+	return $app['twig']->render('game.html');
+});
+
+$app->get('/logout', function() use($app) {
 	return 'thisisgame';
+})
+->bind('logout');
+
+$app->get('/', function() use($app) {
+	return $app['twig']->render('homepage.html');
 });
 
-$app->get('/login', function() use($app) {
-	return 'login form';
+$app->get('/login', function(Request $request) use ($app) {
+    return $app['twig']->render('login.html', array(
+        'error'         => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+    ));
 });
-
 
 $app->run();
