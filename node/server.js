213496ac1645885ef1a0eeaeedc1684918962a23
Dominik Forys <letischoo@gmail.com>
2015-01-29 18:23:18 +0100

    refactor

--- node/server.js
+++ node/server.js
@@ -5,25 +5,41 @@ var server = ws.createServer(function (conn) {
 	connections.push(conn);
     console.log("New connection")
 	console.log("Sum of clients: ", connections.length);
-    conn.on("text", function (str) {
-		data = JSON.parse(str);
-		console.log(data.type);
-		str = data.content;
-		for (var i = 0; i < connections.length; i++) {
-			connections[i].sendText(JSON.stringify({
-				'type': 'global-message',
-				'content': {
-					'name': connections.indexOf(conn),
-					'message': str.toLowerCase()+"."}
-				}
-			));
-		}
+    conn.on("text", function (msg) {
+		data = JSON.parse(msg);
+		handle_message(conn, data);
     })
     conn.on("close", function (code, reason) {
-        console.log("Connection closed")
-		var index = connections.indexOf(conn);
-		console.log(index);
-		connections.splice(index, 1);
-		console.log("Sum of clients: ", connections.length);
+        handle_close(conn, code, reason);
     })
-}).listen(8001)
+}).listen(8001);
+
+function handle_message(conn, data) {
+	if (data.type == 'global-message') {
+		handle_global_message(conn, data);
+	} else {
+		console.log('Wrong message!');
+		console.log(data);
+	}
+}
+
+function handle_global_message(conn, data) {
+	for (var i = 0; i < connections.length; i++) {
+		connections[i].sendText(JSON.stringify({
+			'type': 'global-message',
+			'content': {
+				'name': connections.indexOf(conn),
+				'message': data.content.toLowerCase()+"."}
+			}
+		));
+	}
+}
+
+function handle_close(conn, code, reason) {
+	console.log("Connection closed")
+	console.log(reason);
+	var index = connections.indexOf(conn);
+	console.log(index);
+	connections.splice(index, 1);
+	console.log("Sum of clients: ", connections.length);
+}
